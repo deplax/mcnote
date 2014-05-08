9a163cd4813e4db7f61961d6616c1aa36772e462
Jeong Won Cheol <sturdyegg@gmail.com>
2014-05-08 11:27:50 +0900

    기능구현완료

---
+++ visualstudio/week09/Timer/Timer/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Timer
+{
+        partial class Form1
+        {
+	/// <summary>
+	/// Required designer variable.
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	/// <summary>
+	/// Clean up any resources being used.
+	/// </summary>
+	/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	protected override void Dispose(bool disposing)
+	{
+	        if (disposing && (components != null))
+	        {
+		components.Dispose();
+	        }
+	        base.Dispose(disposing);
+	}
+
+	#region Windows Form Designer generated code
+
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	{
+	        this.components = new System.ComponentModel.Container();
+	        this.label1 = new System.Windows.Forms.Label();
+	        this.groupBox1 = new System.Windows.Forms.GroupBox();
+	        this.textBox3 = new System.Windows.Forms.TextBox();
+	        this.label5 = new System.Windows.Forms.Label();
+	        this.radiotimer = new System.Windows.Forms.RadioButton();
+	        this.textBox2 = new System.Windows.Forms.TextBox();
+	        this.radiostopwatch = new System.Windows.Forms.RadioButton();
+	        this.label4 = new System.Windows.Forms.Label();
+	        this.radiowatch = new System.Windows.Forms.RadioButton();
+	        this.textBox1 = new System.Windows.Forms.TextBox();
+	        this.label3 = new System.Windows.Forms.Label();
+	        this.label2 = new System.Windows.Forms.Label();
+	        this.button1 = new System.Windows.Forms.Button();
+	        this.button2 = new System.Windows.Forms.Button();
+	        this.timerwatch = new System.Windows.Forms.Timer(this.components);
+	        this.timerstopwatch = new System.Windows.Forms.Timer(this.components);
+	        this.timer = new System.Windows.Forms.Timer(this.components);
+	        this.groupBox1.SuspendLayout();
+	        this.SuspendLayout();
+	        // 
+	        // label1
+	        // 
+	        this.label1.AutoSize = true;
+	        this.label1.Font = new System.Drawing.Font("NanumGothicExtraBold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+	        this.label1.Location = new System.Drawing.Point(12, 53);
+	        this.label1.Name = "label1";
+	        this.label1.Size = new System.Drawing.Size(169, 31);
+	        this.label1.TabIndex = 0;
+	        this.label1.Text = "00:00:00:00";
+	        // 
+	        // groupBox1
+	        // 
+	        this.groupBox1.Controls.Add(this.textBox3);
+	        this.groupBox1.Controls.Add(this.label5);
+	        this.groupBox1.Controls.Add(this.radiotimer);
+	        this.groupBox1.Controls.Add(this.textBox2);
+	        this.groupBox1.Controls.Add(this.radiostopwatch);
+	        this.groupBox1.Controls.Add(this.label4);
+	        this.groupBox1.Controls.Add(this.radiowatch);
+	        this.groupBox1.Controls.Add(this.textBox1);
+	        this.groupBox1.Controls.Add(this.label3);
+	        this.groupBox1.Location = new System.Drawing.Point(187, 24);
+	        this.groupBox1.Name = "groupBox1";
+	        this.groupBox1.Size = new System.Drawing.Size(136, 115);
+	        this.groupBox1.TabIndex = 1;
+	        this.groupBox1.TabStop = false;
+	        this.groupBox1.Text = "기능";
+	        // 
+	        // textBox3
+	        // 
+	        this.textBox3.Location = new System.Drawing.Point(89, 88);
+	        this.textBox3.Name = "textBox3";
+	        this.textBox3.Size = new System.Drawing.Size(25, 21);
+	        this.textBox3.TabIndex = 8;
+	        // 
+	        // label5
+	        // 
+	        this.label5.AutoSize = true;
+	        this.label5.Location = new System.Drawing.Point(113, 91);
+	        this.label5.Name = "label5";
+	        this.label5.Size = new System.Drawing.Size(17, 12);
+	        this.label5.TabIndex = 9;
+	        this.label5.Text = "초";
+	        // 
+	        // radiotimer
+	        // 
+	        this.radiotimer.AutoSize = true;
+	        this.radiotimer.Location = new System.Drawing.Point(6, 64);
+	        this.radiotimer.Name = "radiotimer";
+	        this.radiotimer.Size = new System.Drawing.Size(59, 16);
+	        this.radiotimer.TabIndex = 2;
+	        this.radiotimer.TabStop = true;
+	        this.radiotimer.Text = "타이머";
+	        this.radiotimer.UseVisualStyleBackColor = true;
+	        // 
+	        // textBox2
+	        // 
+	        this.textBox2.Location = new System.Drawing.Point(47, 88);
+	        this.textBox2.Name = "textBox2";
+	        this.textBox2.Size = new System.Drawing.Size(25, 21);
+	        this.textBox2.TabIndex = 6;
+	        // 
+	        // radiostopwatch
+	        // 
+	        this.radiostopwatch.AutoSize = true;
+	        this.radiostopwatch.Location = new System.Drawing.Point(6, 42);
+	        this.radiostopwatch.Name = "radiostopwatch";
+	        this.radiostopwatch.Size = new System.Drawing.Size(71, 16);
+	        this.radiostopwatch.TabIndex = 1;
+	        this.radiostopwatch.TabStop = true;
+	        this.radiostopwatch.Text = "스톱워치";
+	        this.radiostopwatch.UseVisualStyleBackColor = true;
+	        // 
+	        // label4
+	        // 
+	        this.label4.AutoSize = true;
+	        this.label4.Location = new System.Drawing.Point(71, 91);
+	        this.label4.Name = "label4";
+	        this.label4.Size = new System.Drawing.Size(17, 12);
+	        this.label4.TabIndex = 7;
+	        this.label4.Text = "분";
+	        // 
+	        // radiowatch
+	        // 
+	        this.radiowatch.AutoSize = true;
+	        this.radiowatch.Location = new System.Drawing.Point(6, 20);
+	        this.radiowatch.Name = "radiowatch";
+	        this.radiowatch.Size = new System.Drawing.Size(47, 16);
+	        this.radiowatch.TabIndex = 0;
+	        this.radiowatch.TabStop = true;
+	        this.radiowatch.Text = "시계";
+	        this.radiowatch.UseVisualStyleBackColor = true;
+	        this.radiowatch.CheckedChanged += new System.EventHandler(this.radiowatch_CheckedChanged);
+	        // 
+	        // textBox1
+	        // 
+	        this.textBox1.Location = new System.Drawing.Point(6, 88);
+	        this.textBox1.Name = "textBox1";
+	        this.textBox1.Size = new System.Drawing.Size(25, 21);
+	        this.textBox1.TabIndex = 3;
+	        // 
+	        // label3
+	        // 
+	        this.label3.AutoSize = true;
+	        this.label3.Location = new System.Drawing.Point(30, 91);
+	        this.label3.Name = "label3";
+	        this.label3.Size = new System.Drawing.Size(17, 12);
+	        this.label3.TabIndex = 5;
+	        this.label3.Text = "시";
+	        // 
+	        // label2
+	        // 
+	        this.label2.AutoSize = true;
+	        this.label2.Location = new System.Drawing.Point(16, 90);
+	        this.label2.Name = "label2";
+	        this.label2.Size = new System.Drawing.Size(161, 12);
+	        this.label2.TabIndex = 2;
+	        this.label2.Text = "--------------------------";
+	        // 
+	        // button1
+	        // 
+	        this.button1.Location = new System.Drawing.Point(14, 24);
+	        this.button1.Name = "button1";
+	        this.button1.Size = new System.Drawing.Size(75, 23);
+	        this.button1.TabIndex = 3;
+	        this.button1.Text = "확인";
+	        this.button1.UseVisualStyleBackColor = true;
+	        this.button1.Click += new System.EventHandler(this.button1_Click);
+	        // 
+	        // button2
+	        // 
+	        this.button2.Location = new System.Drawing.Point(95, 24);
+	        this.button2.Name = "button2";
+	        this.button2.Size = new System.Drawing.Size(75, 23);
+	        this.button2.TabIndex = 4;
+	        this.button2.Text = "리셋";
+	        this.button2.UseVisualStyleBackColor = true;
+	        this.button2.Click += new System.EventHandler(this.button2_Click);
+	        // 
+	        // timerwatch
+	        // 
+	        this.timerwatch.Tick += new System.EventHandler(this.timerwatch_Tick);
+	        // 
+	        // timerstopwatch
+	        // 
+	        this.timerstopwatch.Tick += new System.EventHandler(this.timerstopwatch_Tick);
+	        // 
+	        // timer
+	        // 
+	        this.timer.Tick += new System.EventHandler(this.timer_Tick);
+	        // 
+	        // Form1
+	        // 
+	        this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+	        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+	        this.ClientSize = new System.Drawing.Size(332, 241);
+	        this.Controls.Add(this.button2);
+	        this.Controls.Add(this.button1);
+	        this.Controls.Add(this.label2);
+	        this.Controls.Add(this.groupBox1);
+	        this.Controls.Add(this.label1);
+	        this.Name = "Form1";
+	        this.Text = "쓰잘떼기없는 프로그램";
+	        this.Load += new System.EventHandler(this.Form1_Load);
+	        this.groupBox1.ResumeLayout(false);
+	        this.groupBox1.PerformLayout();
+	        this.ResumeLayout(false);
+	        this.PerformLayout();
+
+	}
+
+	#endregion
+
+	private System.Windows.Forms.Label label1;
+	private System.Windows.Forms.GroupBox groupBox1;
+	private System.Windows.Forms.RadioButton radiotimer;
+	private System.Windows.Forms.RadioButton radiostopwatch;
+	private System.Windows.Forms.RadioButton radiowatch;
+	private System.Windows.Forms.TextBox textBox1;
+	private System.Windows.Forms.Label label2;
+	private System.Windows.Forms.Button button1;
+	private System.Windows.Forms.Button button2;
+	private System.Windows.Forms.Timer timerwatch;
+	private System.Windows.Forms.Timer timerstopwatch;
+	private System.Windows.Forms.Timer timer;
+	private System.Windows.Forms.Label label3;
+	private System.Windows.Forms.TextBox textBox2;
+	private System.Windows.Forms.Label label4;
+	private System.Windows.Forms.TextBox textBox3;
+	private System.Windows.Forms.Label label5;
+        }
+}
+
---
+++ visualstudio/week09/Timer/Timer/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+        public partial class Form1 : Form
+        {
+	int stopWatchStampms = 0;
+	int stopWatchPausems = 0;
+	int stopWatchPauseStart = 0;
+	int stopWatchPauseStop = 0;
+	int stopwatchms = 0;
+	bool stopWatchSW = true;
+	int stopWatchState = 0;			        //0초기, 1작동, 2일시정지
+
+	int timerStampms = 0;
+	int timerTime = 0;
+	int timerPausems = 0;
+	int timerPauseStart = 0;
+	int timerPauseStop = 0;
+	int timerAllms = 0;
+	bool timerSW = true;
+	int timerState = 0;			        //0초기, 1작동, 2일시정지
+
+	public Form1()
+	{
+	        InitializeComponent();
+
+	}
+
+
+	private void Form1_Load(object sender, EventArgs e)
+	{
+	        radiowatch.Checked = true;
+	}
+
+	private void radiowatch_CheckedChanged(object sender, EventArgs e)
+	{
+	        if (radiowatch.Checked)			//시계 선택시
+	        {
+		timerwatch.Interval = 10;
+		timerwatch.Enabled = true;
+		button1.Enabled = false;
+		button2.Enabled = false;
+	        }
+	        else if (radiostopwatch.Checked)		//스톱워치 선택시
+	        {
+		button1.Enabled = true;
+		button2.Enabled = true;
+		if (stopWatchState == 0)			//초기상태
+		{
+		        label1.Text = String.Format("00:00:00:00");
+		        button1.Text = "시작";
+		        button2.Text = "기록";
+		}
+		else if (stopWatchState == 1)		//작동중
+		{
+		        button1.Text = "일시정지";
+		        button2.Text = "기록";
+		}
+		else					//일시정지
+		{
+		        int ms = 0, s = 0, m = 0, h = 0;
+		        timeDevide(ref stopwatchms, ref ms, ref s, ref m, ref h);
+		        label1.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", h, m, s, ms);
+		        button1.Text = "다시시작";
+		        button2.Text = "초기화";
+		}
+	        }
+	        else
+	        {						//타이머 선택시
+		button1.Enabled = true;
+		button2.Enabled = true;
+		if (timerState == 0)
+		{
+		        label1.Text = String.Format("00:00:00:00");
+		        button1.Text = "시작";
+		        button2.Text = "초기화";
+		}
+		else if (timerState == 1)
+		{
+		        button1.Text = "일시정지";
+		        button2.Text = "초기화";
+		}
+		else
+		{
+		        int ms = 0, s = 0, m = 0, h = 0;
+		        timeDevide(ref timerAllms, ref ms, ref s, ref m, ref h);
+		        label1.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", h, m, s, ms);
+		        button1.Text = "다시시작";
+		        button2.Text = "초기화";
+		}
+
+	        }
+	}
+
+	private void GetTimeNow(ref int h, ref int m, ref int s, ref int ms)
+	{
+	        h = System.DateTime.Now.Hour;
+	        m = System.DateTime.Now.Minute;
+	        s = System.DateTime.Now.Second;
+	        ms = System.DateTime.Now.Millisecond;
+	}
+
+	private void timerwatch_Tick(object sender, EventArgs e)			//시계
+	{
+	        int h = 0, m = 0, s = 0, ms = 0;
+	        GetTimeNow(ref h, ref m, ref s, ref ms);
+	        if (radiowatch.Checked)
+		label1.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", h, m, s, ms % 1000 / 10);
+	}
+
+	private void timeDevide(ref int time, ref int ms, ref int s, ref int m, ref int h)
+	{
+	        ms = time % 1000 / 10;
+	        s = time / 1000 % 60;
+	        m = time / 1000 / 60 % 60;
+	        h = time / 1000 / 60 / 60 % 60;
+	}
+
+	private void timerstopwatch_Tick(object sender, EventArgs e)		        //스톱워치
+	{
+	        int h = 0, m = 0, s = 0, ms = 0;
+	        GetTimeNow(ref h, ref m, ref s, ref ms);
+
+	        if (stopWatchSW)
+	        {
+		stopWatchStampms = ms + s * 1000 + m * 1000 * 60 + h * 1000 * 60 * 60;	//타이머 시작시간 스탬프
+		stopWatchSW = false;
+	        }
+
+	        int currentms = ms + s * 1000 + m * 1000 * 60 + h * 1000 * 60 * 60;			//현재시간 스탬프
+
+	        stopwatchms = currentms - stopWatchStampms - stopWatchPausems;
+	        int stms = 0, sts = 0, stm = 0, sth = 0;
+	        timeDevide(ref stopwatchms, ref stms, ref sts, ref stm, ref sth);
+
+	        if (radiostopwatch.Checked)
+		label1.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", sth, stm, sts, stms);
+
+	}
+
+	private void timer_Tick(object sender, EventArgs e)				//타이머
+	{
+	        int h = 0, m = 0, s = 0, ms = 0;
+	        GetTimeNow(ref h, ref m, ref s, ref ms);
+	        if (timerSW)
+	        {
+		timerStampms = ms + s * 1000 + m * 1000 * 60 + h * 1000 * 60 * 60;
+		timerSW = false;
+	        }
+
+
+	        int currentms = ms + s * 1000 + m * 1000 * 60 + h * 1000 * 60 * 60;
+	        timerAllms = timerTime - (currentms - timerStampms) + timerPausems;
+	        int stms = 0, sts = 0, stm = 0, sth = 0;
+
+	        if (timerAllms <= 0)
+	        {
+		timerAllms = 0;
+		timerState = 0;
+		timerStampms = 0;
+		timerPausems = 0;
+		timerPauseStart = 0;
+		timerPauseStop = 0;
+		timerState = 0;
+		button1.Text = "시작";
+		button2.Text = "초기화";
+		timer.Enabled = false;
+		timerSW = true;
+		//소리 들려야지요...
+	        }
+
+	        timeDevide(ref timerAllms, ref stms, ref sts, ref stm, ref sth);
+
+
+
+	        if (radiotimer.Checked)
+		label1.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", sth, stm, sts, stms);
+	}
+
+	private void button1_Click(object sender, EventArgs e)
+	{
+	        if (radiowatch.Checked) { }		        //시계일때
+	        else if (radiostopwatch.Checked)	        //스톱워치때
+	        {
+		if (stopWatchState == 0)		        //초기상태	중지      기록
+		{
+		        timerstopwatch.Interval = 10;
+		        timerstopwatch.Enabled = true;
+		        button1.Text = "일시정지";
+		        button2.Text = "기록";
+		        stopWatchState = 1;
+		}
+		else if (stopWatchState == 1)	        //작동중	일시정지    기록
+		{
+		        timerstopwatch.Enabled = false;
+		        button1.Text = "다시시작";
+		        button2.Text = "초기화";
+		        stopWatchState = 2;
+
+		        int h = 0, m = 0, s = 0, ms = 0;
+		        GetTimeNow(ref h, ref m, ref s, ref ms);
+		        stopWatchPauseStart = ms + s * 1000 + m * 1000 * 60 + h * 1000 * 60 * 60;
+
+		}
+		else
+		{				        //일시정지	다시시작    초기화
+		        timerstopwatch.Interval = 10;
+		        timerstopwatch.Enabled = true;
+		        button1.Text = "일시정지";
+		        button2.Text = "기록";
+		        stopWatchState = 1;
+
+		        int h = 0, m = 0, s = 0, ms = 0;
+		        GetTimeNow(ref h, ref m, ref s, ref ms);
+		        stopWatchPauseStop = ms + s * 1000 + m * 1000 * 60 + h * 1000 * 60 * 60;
+		        stopWatchPausems += (stopWatchPauseStop - stopWatchPauseStart);
+
+		}
+
+	        }
+	        else
+	        {					        //타이머 때
+		if (timerState == 0)		        //정지상태
+		{
+		        timer.Interval = 10;
+		        timer.Enabled = true;
+
+		        button1.Text = "일시정지";
+		        button2.Text = "초기화";
+		        timerState = 1;
+		        if (textBox1.Text == "")
+			textBox1.Text = "0";
+		        if (textBox2.Text == "")
+			textBox2.Text = "0";
+		        if (textBox3.Text == "")
+			textBox3.Text = "0";
+		        int timerh = Convert.ToInt32(textBox1.Text);
+		        int timerm = Convert.ToInt32(textBox2.Text);
+		        int timers = Convert.ToInt32(textBox3.Text);
+
+		        timerTime = timers * 1000 + timerm * 1000 * 60 + timerh * 1000 * 60 * 60;
+		}
+		else if (timerState == 1)		//작동중
+		{
+		        timer.Enabled = false;
+		        button1.Text = "다시시작";
+		        button2.Text = "초기화";
+		        timerState = 2;
+		        int h = 0, m = 0, s = 0, ms = 0;
+		        GetTimeNow(ref h, ref m, ref s, ref ms);
+		        timerPauseStart = ms + s * 1000 + m * 1000 * 60 + h * 1000 * 60 * 60;
+		}
+		else				//일시정지
+		{
+		        timer.Interval = 10;
+		        timer.Enabled = true;
+		        button1.Text = "일시정지";
+		        button2.Text = "초기화";
+		        timerState = 1;
+
+		        int h = 0, m = 0, s = 0, ms = 0;
+		        GetTimeNow(ref h, ref m, ref s, ref ms);
+		        timerPauseStop = ms + s * 1000 + m * 1000 * 60 + h * 1000 * 60 * 60;
+		        timerPausems += (timerPauseStop - timerPauseStart);
+		}
+
+	        }
+	}
+	private void button2_Click(object sender, EventArgs e)
+	{
+	        if (radiowatch.Checked) { }		        //시계일때
+	        else if (radiostopwatch.Checked)	        //스톱워치때
+	        {
+		if (stopWatchState == 0) { }	        //초기상태
+		else if (stopWatchState == 1)	        //작동중	기록
+		{
+		        label2.Text += "\r\n" + label1.Text;
+		}
+		else				        //일시정지    초기화
+		{
+		        label1.Text = String.Format("00:00:00:00");
+		        label2.Text = "--------------------------";
+		        stopWatchStampms = 0;
+		        stopWatchPausems = 0;
+		        stopWatchPauseStart = 0;
+		        stopWatchPauseStop = 0;
+		        stopWatchState = 0;
+		        button1.Text = "시작";
+		        button2.Text = "기록";
+		        timerstopwatch.Enabled = false;
+		        stopWatchSW = true;
+		}
+
+	        }
+	        else
+	        {					          //타이머 때
+		label1.Text = String.Format("00:00:00:00");
+		label2.Text = "--------------------------";
+		timerStampms = 0;
+		timerPausems = 0;
+		timerPauseStart = 0;
+		timerPauseStop = 0;
+		timerState = 0;
+		button1.Text = "시작";
+		button2.Text = "초기화";
+		timer.Enabled = false;
+		timerSW = true;
+
+	        }
+	}
+
+
+        }
+}
