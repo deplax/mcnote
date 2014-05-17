918daa10cfdbb9453444943c97c6850755335439
Jeong Won Cheol <sturdyegg@gmail.com>
2014-05-17 18:35:21 +0900

    xml_load

--- visualstudio/week11/ImageViewer/ImageViewer/Form1.Designer.cs
+++ visualstudio/week11/ImageViewer/ImageViewer/Form1.Designer.cs
@@ -28,59 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 335);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "불러오기";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(93, 335);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "저장하기";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(420, 303);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 370);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Image";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
+	        this.button1 = new System.Windows.Forms.Button();
+	        this.button2 = new System.Windows.Forms.Button();
+	        this.pictureBox1 = new System.Windows.Forms.PictureBox();
+	        this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+	        this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+	        this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+	        this.button3 = new System.Windows.Forms.Button();
+	        this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+	        this.button4 = new System.Windows.Forms.Button();
+	        ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+	        this.SuspendLayout();
+	        // 
+	        // button1
+	        // 
+	        this.button1.Location = new System.Drawing.Point(12, 575);
+	        this.button1.Name = "button1";
+	        this.button1.Size = new System.Drawing.Size(75, 23);
+	        this.button1.TabIndex = 0;
+	        this.button1.Text = "불러오기";
+	        this.button1.UseVisualStyleBackColor = true;
+	        this.button1.Click += new System.EventHandler(this.button1_Click);
+	        // 
+	        // button2
+	        // 
+	        this.button2.Location = new System.Drawing.Point(93, 575);
+	        this.button2.Name = "button2";
+	        this.button2.Size = new System.Drawing.Size(98, 23);
+	        this.button2.TabIndex = 1;
+	        this.button2.Text = "XML 저장하기";
+	        this.button2.UseVisualStyleBackColor = true;
+	        this.button2.Click += new System.EventHandler(this.button2_Click);
+	        // 
+	        // pictureBox1
+	        // 
+	        this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+	        this.pictureBox1.Name = "pictureBox1";
+	        this.pictureBox1.Size = new System.Drawing.Size(783, 557);
+	        this.pictureBox1.TabIndex = 2;
+	        this.pictureBox1.TabStop = false;
+	        // 
+	        // openFileDialog1
+	        // 
+	        this.openFileDialog1.FileName = "openFileDialog1";
+	        // 
+	        // button3
+	        // 
+	        this.button3.Location = new System.Drawing.Point(278, 575);
+	        this.button3.Name = "button3";
+	        this.button3.Size = new System.Drawing.Size(98, 23);
+	        this.button3.TabIndex = 3;
+	        this.button3.Text = "XML 불러오기";
+	        this.button3.UseVisualStyleBackColor = true;
+	        this.button3.Click += new System.EventHandler(this.button3_Click);
+	        // 
+	        // openFileDialog2
+	        // 
+	        this.openFileDialog2.FileName = "openFileDialog2";
+	        // 
+	        // button4
+	        // 
+	        this.button4.Location = new System.Drawing.Point(197, 575);
+	        this.button4.Name = "button4";
+	        this.button4.Size = new System.Drawing.Size(75, 23);
+	        this.button4.TabIndex = 4;
+	        this.button4.Text = "지우기";
+	        this.button4.UseVisualStyleBackColor = true;
+	        this.button4.Click += new System.EventHandler(this.button4_Click);
+	        // 
+	        // Form1
+	        // 
+	        this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+	        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+	        this.ClientSize = new System.Drawing.Size(807, 610);
+	        this.Controls.Add(this.button4);
+	        this.Controls.Add(this.button3);
+	        this.Controls.Add(this.pictureBox1);
+	        this.Controls.Add(this.button2);
+	        this.Controls.Add(this.button1);
+	        this.Name = "Form1";
+	        this.Text = "Image";
+	        this.Load += new System.EventHandler(this.Form1_Load);
+	        ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+	        this.ResumeLayout(false);
 
         }
 
@@ -92,6 +122,9 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- visualstudio/week11/ImageViewer/ImageViewer/Form1.cs
+++ visualstudio/week11/ImageViewer/ImageViewer/Form1.cs
@@ -14,49 +14,89 @@ using System.Xml.Serialization;
 
 namespace ImageViewer
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+        public partial class Form1 : Form
         {
-            InitializeComponent();
-            openFileDialog1.Filter = "JPG(*.jpg)|*.jpg";
-            saveFileDialog1.Filter = "저장파일(*.xml)|*.xml|전부(*.*)|*.*";
-        }
+	public Form1()
+	{
+	        InitializeComponent();
+	        openFileDialog1.Filter = "JPG(*.jpg)|*.jpg";
+	        saveFileDialog1.Filter = "저장파일(*.xml)|*.xml|전부(*.*)|*.*";
+	        openFileDialog2.Filter = "XML(*.xml)|*.xml";
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            if(!(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK))
-            {
-                return;
-            }
-            Image img = Image.FromFile(openFileDialog1.FileName);
-            pictureBox1.Width = img.Width;
-            pictureBox1.Height = img.Height;
-            pictureBox1.Image = img;
-            pictureBox1.Refresh();
-        }
+	}
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Bitmap bmp = new Bitmap(pictureBox1.Image);
-            ImageData ImgData = new ImageData();
-            ImgData.SetSize(bmp.Width, bmp.Height);
-            for(int i = 0; i < bmp.Height; i++)
-            {
-                for(int j = 0; j < bmp.Width; j++)
-                {
-                    ImgData.pixel[i * ImgData.width + j] = bmp.GetPixel(j, i).ToArgb();
-                }
-            
-            }
-            if (saveFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK)
-            {
-                return;
-            }
-            XmlSerializer xs = new XmlSerializer(typeof(ImageData));
-            StreamWriter sw = new StreamWriter(saveFileDialog1.FileName, false);
-            xs.Serialize(sw, ImgData);
-            sw.Close();
+	private void Form1_Load(object sender, EventArgs e)
+	{
+
+	}
+
+	private void button2_Click(object sender, EventArgs e)		        //xml 저장하기
+	{
+	        Bitmap bmp = new Bitmap(pictureBox1.Image);		        //비트맵 형식으로 픽쳐박스 이미지 받음
+	        ImageData ImgData = new ImageData();			        //이미지 데이터 클래스 생성
+	        ImgData.SetSize(bmp.Width, bmp.Height);			        //가로 세로 받아와서 입력
+	        for (int i = 0; i < bmp.Height; i++)				        //세로루프
+	        {
+		for (int j = 0; j < bmp.Width; j++)				        //가로루프
+		{
+		        ImgData.pixel[i * ImgData.width + j] = bmp.GetPixel(j, i).ToArgb();	        //픽셀당 색상값 넣기
+		}
+
+	        }
+	        if (saveFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+	        {
+		return;
+	        }
+	        XmlSerializer xs = new XmlSerializer(typeof(ImageData));		        //이미지데이터 타입으로 시리얼라이즈 
+	        StreamWriter sw = new StreamWriter(saveFileDialog1.FileName, false);	        //스트림 라이터
+	        xs.Serialize(sw, ImgData);						        //기록
+	        sw.Close();							        //닫기
+	}
+
+	private void button1_Click(object sender, EventArgs e)		        //불러오기
+	{
+	        if (!(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK))
+	        {
+		return;
+	        }
+	        Image img = Image.FromFile(openFileDialog1.FileName);
+	        pictureBox1.Width = img.Width;
+	        pictureBox1.Height = img.Height;
+	        pictureBox1.Image = img;
+	        pictureBox1.Refresh();
+	}
+
+	private void button3_Click(object sender, EventArgs e)		        //xml불러오기
+	{
+	        if (!(openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK))
+	        {
+		return;
+	        }
+	        StreamReader sr = new StreamReader(openFileDialog2.FileName, Encoding.GetEncoding("euc-kr"));
+	        XmlSerializer xs = new XmlSerializer(typeof(ImageData));
+	        ImageData imgData = (ImageData)xs.Deserialize(sr);
+	        sr.Close();
+
+	        Bitmap bmp = new Bitmap(imgData.width, imgData.height);
+
+	        for (int i = 0; i < imgData.height; i++)
+	        {
+		for (int j = 0; j < imgData.width; j++)
+		{
+		        bmp.SetPixel(j, i, Color.FromArgb(imgData.pixel[i * imgData.width + j]));
+		        imgData.pixel[i * imgData.width + j] = bmp.GetPixel(j, i).ToArgb();
+		}
+	        }
+
+	        pictureBox1.Image = bmp;
+
+
+
+	}
+
+	private void button4_Click(object sender, EventArgs e)
+	{
+	        pictureBox1.Image = null;
+	}
         }
-    }
 }
